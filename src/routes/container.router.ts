fe65bc9afd07dcffd7e058c9352f6ecad0a7a444
gon2410x <gonzalo.virtual.2020@gmail.com>
2023-09-02 17:26:04 -0300

    adding EndPoint high, low and modification of Container

--- src/routes/container.router.ts
+++ src/routes/container.router.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import {getContainers} from "../controllers/container.controller";
+import {getContainers, createContainer, updateContainer, deleteContainer} from "../controllers/container.controller";
 
 const router = Router();
 
 router.get("/containers", getContainers);
-
+router.post("/containers", createContainer);
+router.put("/containers/:id", updateContainer);
+router.delete("/containers/:id", deleteContainer);
 
 export default router;
