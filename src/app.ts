88b63527f4f5fdb92cfcf64f567bb49c77b94ea0
gon2410x <gonzalo.virtual.2020@gmail.com>
2023-09-04 11:34:20 -0300

    add cors configuration

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ import passport from 'passport'
 
 const app = express()
 app.use(morgan('dev'));
-app.use(cors());
+app.use(cors({ origin: true, credentials: true  }));
 app.use(express.json());
 
 app.use(express.urlencoded({extended: false}));
