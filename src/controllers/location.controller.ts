1ed30090c11b8c66b92401cbf309cb43400598a9
sebamiranda <carlos.seba.miranda@gmail.com>
2023-09-05 20:44:18 -0300

    camnios en consulta location

--- src/controllers/location.controller.ts
+++ src/controllers/location.controller.ts
@@ -2,17 +2,18 @@ import { Request, Response } from "express";
 
 
 import { Department } from "../entity/Department";
+import { Location } from "../entity/Location";
 
 
 export const getLocation = async (req: Request, res: Response) => {
   
     try {
 
-        const { id } = req.body;
+  //      const { id } = req.body;
        
-        const loca = await Department.find({ 
-          where: { id_department: parseInt(id) },
-          relations:['location']
+        const loca = await Location.find({ 
+ //         where: { id_department: parseInt(id) },
+          relations:['container','organization','department']
         });
        
       return res.status(200).json(loca);
